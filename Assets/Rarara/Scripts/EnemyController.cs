d6447d4300aec366305f244567d981522425e3b6
ららら <sinralongqiu@gmail.com>
2022-06-22 02:07:23 +0900

    敵の移動処理の追加

--- Assets/Rarara/Scripts/EnemyController.cs
+++ Assets/Rarara/Scripts/EnemyController.cs
@@ -4,71 +4,131 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    private Rigidbody2D rb;
-    private Animator anim;
-    private float waitCounter = 0f;
-    private float moveCounter = 0f;
+    [SerializeField] private float speed = 3f; //移動速度
+    [SerializeField] private float moveTime = 2f; //移動時間
+    [SerializeField] private float waitTime = 2f; //待機時間
+    [SerializeField] private BoxCollider2D area; //動ける範囲
+
+    private float timeCounter = 0f;
+    private float colXHalf;
     private bool isWait = true;
     private bool isMove = false;
-    private Vector2 moveDir;
+    private Vector2 moveDir; //進行方向
 
-    [SerializeField] private float moveSpeed;
-    [SerializeField] private float walkTime;
-    [SerializeField] private float waitTime;
-    [SerializeField] private BoxCollider2D moveArea;
+    private Rigidbody2D rb;
+    private Animator anim;
+    private BoxCollider2D col;
 
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        col = GetComponent<BoxCollider2D>();
+        colXHalf = col.size.x / 2;
+        moveDir = RandomDirection();
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
+    private void FixedUpdate()
     {
-        moveDir = new Vector2(Random.Range(-1f,1f), Random.Range(-1f,1f));
-        moveDir.Normalize();
-        rb.velocity = moveDir * moveSpeed;
-        Anim(moveDir);
+
+        if ((rb.position.x < area.bounds.min.x + colXHalf) || (rb.position.x > area.bounds.max.x - colXHalf) || (rb.position.y < area.bounds.min.y + colXHalf) || (rb.position.y > area.bounds.max.y - colXHalf))
+            Turn();
+
+        if (isWait)
+            Wait();
+        else if (isMove)
+            Move();
     }
 
-    void Wait()
+    //待機処理
+    private void Wait()
     {
-        if (waitTime > waitCounter)
+        if (waitTime > timeCounter)
         {
-            waitCounter += Time.deltaTime;
+            timeCounter += Time.deltaTime;
             rb.velocity = Vector2.zero;
         }
         else
         {
             isWait = false;
-            waitCounter = 0f;
+            isMove = true;
+            timeCounter = 0f;
         }
     }
 
-    public void Anim(Vector2 dir)
+    //アニメーション推移
+    private void Animate()
     {
-        if (dir.x == 0 && dir.y == -1)
-        {
-            anim.SetFloat("X", 0);
-            anim.SetFloat("Y", -1f);
-        }
-        if (dir.x == 0 && dir.y == 1)
+        if (moveDir == Vector2.up)
         {
             anim.SetFloat("X", 0);
             anim.SetFloat("Y", 1f);
         }
-        if (dir.x == -1 && dir.y == 0)
+        else if (moveDir == Vector2.down)
+        {
+            anim.SetFloat("X", 0);
+            anim.SetFloat("Y", -1f);
+        }
+        else if (moveDir == Vector2.left)
         {
             anim.SetFloat("X", -1f);
             anim.SetFloat("Y", 0);
         }
-        if (dir.x == 1 && dir.y == 0)
+        else if (moveDir == Vector2.right)
         {
             anim.SetFloat("X", 1f);
             anim.SetFloat("Y", 0);
         }
+    }
+
+    //ランダムに上下左右の方向を生成
+    private Vector2 RandomDirection()
+    {
+        switch(Random.Range(0, 4))
+        {
+            default:
+            case 0:
+                return Vector2.up;
+            case 1:
+                return Vector2.down;
+            case 2:
+                return Vector2.left;
+            case 3:
+                return Vector2.right;
+        }
+    }
+
+    //移動処理
+    private void Move()
+    {
+        if(moveTime > timeCounter)
+        {
+            timeCounter += Time.deltaTime;
+            rb.velocity = moveDir * speed;
+            Animate();
+        }
+        else
+        {
+            moveDir = RandomDirection();
+            isMove = false;
+            isWait = true;
+            timeCounter = 0f;
+        }
+    }
+
+    //ターンする処理
+    private void Turn()
+    {
+        if (moveDir == Vector2.up)
+            rb.position = new Vector2(rb.position.x, area.bounds.max.y - colXHalf);
+        else if (moveDir == Vector2.down)
+            rb.position = new Vector2(rb.position.x, area.bounds.min.y + colXHalf);
+        else if (moveDir == Vector2.left)
+            rb.position = new Vector2(area.bounds.min.x + colXHalf, rb.position.y);
+        else if (moveDir == Vector2.right)
+            rb.position = new Vector2(area.bounds.max.x - colXHalf, rb.position.y);
+
+        moveDir = -moveDir;
     }
 }
